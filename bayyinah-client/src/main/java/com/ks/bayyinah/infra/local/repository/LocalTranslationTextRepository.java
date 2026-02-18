7ad7edce4529df7b53a211bde341167108839999
Mohammad Kamrul Hasan <kamrulhasan59246@gmail.com>
2026-02-19 00:58:00 +0600

    feat: fixed spacing issue in sql queries

--- bayyinah-client/src/main/java/com/ks/bayyinah/infra/local/repository/LocalTranslationTextRepository.java
+++ bayyinah-client/src/main/java/com/ks/bayyinah/infra/local/repository/LocalTranslationTextRepository.java
@@ -68,11 +68,11 @@ public class LocalTranslationTextRepository extends LocalRespository implements 
   public Page<TranslationText> findTranslationsByChapterId(int chapterId, PageRequest pageRequest) {
     try {
       try (var connection = getConnection();
-          var pstmt = connection.prepareStatement("SELECT tt.* from translation_text tt" +
+          var pstmt = connection.prepareStatement("SELECT tt.* from translation_text tt " +
               "JOIN verses v ON tt.verse_id = v.id" + "WHERE surah_id = ? LIMIT ? OFFSET ?");) {
         pstmt.setInt(1, chapterId);
         pstmt.setInt(2, pageRequest.getPageSize());
-        pstmt.setInt(3, pageRequest.getPage() * pageRequest.getPageSize());
+        pstmt.setInt(3, (pageRequest.getPage() - 1) * pageRequest.getPageSize());
         int totalElements = countTranslationsByChapterId(chapterId);
         try (var resultSet = pstmt.executeQuery()) {
           List<TranslationText> translations = new ArrayList<>();
@@ -84,7 +84,9 @@ public class LocalTranslationTextRepository extends LocalRespository implements 
             translationText.setText(resultSet.getString("text"));
             translations.add(translationText);
           }
-          return totalElements > 0 ? new Page<TranslationText>(translations, pageRequest.getPage(), pageRequest.getPageSize(), totalElements) : Page.empty();
+          return totalElements > 0
+              ? new Page<TranslationText>(translations, pageRequest.getPage(), pageRequest.getPageSize(), totalElements)
+              : Page.empty();
         }
       }
     } catch (Exception e) {
@@ -96,7 +98,7 @@ public class LocalTranslationTextRepository extends LocalRespository implements 
   private int countTranslationsByChapterId(int chapterId) {
     try {
       try (var connection = getConnection();
-          var pstmt = connection.prepareStatement("SELECT COUNT(*) as total FROM translation_text tt" +
+          var pstmt = connection.prepareStatement("SELECT COUNT(*) as total FROM translation_text tt " +
               "JOIN verses v ON tt.verse_id = v.id" + "WHERE surah_id = ?");) {
         pstmt.setInt(1, chapterId);
         try (var resultSet = pstmt.executeQuery()) {
