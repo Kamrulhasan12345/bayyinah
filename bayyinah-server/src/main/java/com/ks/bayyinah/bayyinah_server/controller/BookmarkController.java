a1a068daf65facb7cf08b199b11931b70fd836da
Mohammad Kamrul Hasan <kamrulhasan59246@gmail.com>
2026-03-08 21:45:34 +0600

    refactor: removed Param import

--- bayyinah-server/src/main/java/com/ks/bayyinah/bayyinah_server/controller/BookmarkController.java
+++ bayyinah-server/src/main/java/com/ks/bayyinah/bayyinah_server/controller/BookmarkController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
