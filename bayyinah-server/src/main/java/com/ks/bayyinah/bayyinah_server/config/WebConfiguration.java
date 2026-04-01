23802abcd3d2dc4e1fbb7b087713896db75c8451
Mohammad Kamrul Hasan <kamrulhasan59246@gmail.com>
2026-04-02 02:04:53 +0600

    fix: fixed typo

--- bayyinah-server/src/main/java/com/ks/bayyinah/bayyinah_server/config/WebConfiguration.java
+++ bayyinah-server/src/main/java/com/ks/bayyinah/bayyinah_server/config/WebConfiguration.java
@@ -5,12 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration {
-  `@Bean`
+  @Bean
   public CorsFilter corsFilter() {
     CorsConfiguration config = new CorsConfiguration();
     config.addAllowedOriginPattern("*");
@@ -24,5 +22,4 @@ public class WebConfiguration {
 
     return new CorsFilter(source);
   }
-}
 }
